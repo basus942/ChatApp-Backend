0b8116eb93c730220904743b7d6545b58415184e
Subhajit basu <basus942@gmail.com>
2023-09-16 01:01:27 +0530

    added stripe payment route with some error

--- routes/conversationRoute.js
+++ routes/conversationRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 
 const {
   newConversation,
@@ -12,54 +11,5 @@ router.post("/", newConversation);
 
 router.get("/:userId", conversationWithUserId);
 router.get("/:firstUserId/:secondUserId", conversationOfTwoUsers);
-=======
-const Conversations = require("../models/conversationModel");
-const router = express.Router();
-
-router.post("/", async (req, res, next) => {
-  const newConversation = new Conversations({
-    members: [req.body.senderId, req.body.receiverId],
-  });
-
-  try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/:userId", async (req, res, next) => {
-  try {
-    const conversations = await Conversations.find({
-      members: { $in: [req.params.userId] },
-    });
-    res.status(200).json(conversations);
-  } catch (error) {
-    next(error);
-  }
-});
-router.get("/:firstUserId/:secondUserId", async (req, res, next) => {
-  try {
-    const conversations = await Conversations.findOne({
-      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
-    });
-
-    if (!conversations) {
-      // If no conversation is found, create a new conversation
-      const newConversation = new Conversations({
-        members: [req.params.firstUserId, req.params.secondUserId],
-      });
-
-      const savedConversation = await newConversation.save();
-      res.status(200).json(savedConversation);
-    } else {
-      res.status(200).json(conversations);
-    }
-  } catch (error) {
-    next(error);
-  }
-});
->>>>>>> 8d1d1bada8fdb8a1f0185d8bbebebdbfb722a1ab
 
 module.exports = router;
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 
 const {
   newMessage,
@@ -9,29 +8,5 @@ const {
 const router = express.Router();
 router.post("/", newMessage);
 router.get("/:conversationId", messagesUsingConversationId);
-=======
-const Messages = require("../models/messageModel");
-
-const router = express.Router();
-router.post("/", async (req, res, next) => {
-  const newMessage = new Messages(req.body);
-  try {
-    const savedMesssage = await newMessage.save();
-    res.status(200).json(savedMesssage);
-  } catch (error) {
-    next(error);
-  }
-});
-router.get("/:conversationId", async (req, res, next) => {
-  try {
-    const Messsages = await Messages.find({
-      conversationId: req.params.conversationId,
-    });
-    res.status(200).json(Messsages);
-  } catch (error) {
-    next(error);
-  }
-});
->>>>>>> 8d1d1bada8fdb8a1f0185d8bbebebdbfb722a1ab
 
 module.exports = router;
